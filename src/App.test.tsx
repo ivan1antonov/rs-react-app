2660ffcd03b6f21f0e10ef0678d1da47fa33fa07
Ivan Antonov <ivan1antonov@gmail.com>
2025-07-20 17:07:07 +0400

    feat: add app test and add command start vitetest with husky

---
+++ src/App.test.tsx
@@ -0,0 +1,50 @@
+import { describe, it, vi, beforeEach, afterEach, expect } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { mockResults } from './test-utils/localStorage';
+import * as services from './services/services';
+import type { ApiResponse } from './types/types';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+
+describe('App component', () => {
+  beforeEach(() => {
+    vi.spyOn(services, 'getResults').mockResolvedValue(mockResults);
+    localStorage.clear();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('calls getResults on mount and displays content after loading', async () => {
+    render(<App />);
+    expect(screen.getByAltText('loading...')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText('Luke Skywalker')).toBeInTheDocument();
+    });
+    expect(services.getResults).toHaveBeenCalledWith('');
+  });
+  it('shows loader while fetching data', async () => {
+    const fetchPromise = new Promise<ApiResponse>(() => {});
+    vi.spyOn(services, 'getResults').mockImplementation(() => fetchPromise);
+    localStorage.clear();
+
+    render(<App />);
+    expect(screen.getByAltText('loading...')).toBeInTheDocument();
+  });
+  it('updates input value and triggers search on button click', async () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText('Do you want find anyone?');
+    const button = screen.getByRole('button', { name: /search/i });
+
+    await userEvent.type(input, 'Luke Skywalker');
+    expect(input).toHaveValue('Luke Skywalker');
+
+    await userEvent.click(button);
+
+    expect(services.getResults).toHaveBeenCalledWith('Luke Skywalker');
+
+    expect(input).toHaveValue('');
+  });
+});
--- src/services/services.test.tsx
+++ src/services/services.test.tsx
@@ -1,36 +1,28 @@
 import { getResults } from './services';
 import { describe, it, expect, vi, beforeEach, afterEach, type Mock } from 'vitest';
+import { localStorageMock, resetLocalStorageMock } from '../test-utils/localStorage';
+import { mockResponseName } from '../test-utils/localStorage';
 
 describe('getResults', () => {
   const original = window.fetch;
   beforeEach(() => {
     window.fetch = vi.fn();
 
-    const localStorageMock: Partial<Storage> = {
-      setItem: vi.fn(),
-      getItem: vi.fn(),
-      removeItem: vi.fn(),
-      clear: vi.fn(),
-      key: vi.fn(),
-      length: 0,
-    };
-
     Object.defineProperty(window, 'localStorage', {
       value: localStorageMock,
       writable: true,
     });
   });
+
   afterEach(() => {
     vi.resetAllMocks();
+    resetLocalStorageMock();
     window.fetch = original;
   });
-  it('fetches data and stores query in localStorage', async () => {
-    const mockResponse = {
-      results: [{ name: 'Luke Skywalker' }],
-    };
 
-    (fetch as unknown as ReturnType<typeof vi.fn>).mockResolvedValue({
-      json: vi.fn().mockResolvedValue(mockResponse),
+  it('fetches data and stores query in localStorage', async () => {
+    (window.fetch as Mock).mockResolvedValue({
+      json: vi.fn().mockResolvedValue(mockResponseName),
     });
 
     const query = 'Luke';
@@ -38,7 +30,7 @@ describe('getResults', () => {
 
     expect(fetch).toHaveBeenCalledWith(`https://swapi.py4e.com/api/people/?search=${query}`);
     expect(localStorage.setItem).toHaveBeenCalledWith('results', query);
-    expect(data).toEqual(mockResponse);
+    expect(data).toEqual(mockResponseName);
   });
 
   it('throws error if fetch fails', async () => {
