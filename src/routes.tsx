21a2684c7cfa4d3e2c9c30b46001f99647e3cd7f
Ivan Antonov <ivan1antonov@gmail.com>
2025-07-30 17:33:31 +0400

    init: create branch

---
+++ src/routes.tsx
@@ -0,0 +1,28 @@
+// import { createBrowserRouter } from 'react-router-dom';
+
+// export const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <App />,
+//         children: [
+//           {
+//             path: '/characters/:id',
+//             element: <CharacterDetails />,
+//           },
+//         ],
+//       },
+//       {
+//         path: '/about',
+//         element: <About />,
+//       },
+//       {
+//         path: '*',
+//         element: <Page404 />,
+//       },
+//     ],
+//   },
+// ]);
